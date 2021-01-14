48a10d0993dcf8d19b770113c9e40710ed45768b
ctwillson <anti.c@foxmail.com>
2021-01-14 23:37:06 +0800

    coroutine init

--- pysnowball/utls.py
+++ pysnowball/utls.py
@@ -4,8 +4,10 @@ import json
 
 import pysnowball.cons as cons
 import pysnowball.token as token
+import aiohttp
+import asyncio
 
-def fetch(url):
+async def fetch_async(url):
     HEADERS = {'Host': 'stock.xueqiu.com',
                'Accept': 'application/json',
                'Cookie': token.get_token(),
@@ -13,7 +15,26 @@ def fetch(url):
                'Accept-Language': 'zh-Hans-CN;q=1, ja-JP;q=0.9',
                'Accept-Encoding': 'br, gzip, deflate',
                'Connection': 'keep-alive'}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url,headers=HEADERS) as response:
 
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.text()
+            print("Body:", html, "...")
+
+def fetch(url,is_async = False):
+    HEADERS = {'Host': 'stock.xueqiu.com',
+               'Accept': 'application/json',
+               'Cookie': token.get_token(),
+               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+               'Accept-Language': 'zh-Hans-CN;q=1, ja-JP;q=0.9',
+               'Accept-Encoding': 'br, gzip, deflate',
+               'Connection': 'keep-alive'}
+    if(is_async):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(fetch_async(url))
     response = requests.get(url,headers=HEADERS)
 
     # print(url)
