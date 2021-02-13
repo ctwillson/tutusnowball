7f9fd9a6e30ff0c83b3cced702c6dc5bb123363e
ctwillson <anti.c@foxmail.com>
2021-02-13 23:49:34 +0800

    bug fix

--- pysnowball/history_kline.py
+++ pysnowball/history_kline.py
@@ -1,13 +1,19 @@
 from pysnowball import api_ref
 from pysnowball import utls
 
-def history_kline(symbol='SZ000001',begin='1502035200000',period='day',type='next',count='1000',indicator='kline'):
+'''
+type_name:
+    - before: 前复权(split-adjusted?)
+    - normal: 不复权
+    - after: 后复权
+'''
+def history_kline(symbol='SZ000001',begin='1502035200000',period='day',type_name='before',count='1000',indicator='kline'):
     url = api_ref.history_kline
     query = {
         'symbol': symbol,
         'begin': begin,
         'period': period,
-        'type': type,
+        'type': type_name,
         'count': count,
         'indicator': indicator
     }
