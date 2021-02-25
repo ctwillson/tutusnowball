5c62dd537f94bcbb06616e84d3a3d806b40f66a1
ctwillson <anti.c@foxmail.com>
2021-02-26 00:40:31 +0800

    update shell

--- shell/addkline_csv.sh
+++ shell/addkline_csv.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# crontab eg: 38 00 * * *  /home/ubuntu/stock/tutusnowball/shell/addkline_csv.sh
+#put your token
+export TS_TOKEN=''
+#put your cookie
+export export XUEQIULOGINCOOKIE=''
 CURDIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd )
 PYTHONDIR=$(which python)
 $PYTHONDIR $CURDIR/../addkline_csv.py --all
