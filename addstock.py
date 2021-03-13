9b8c646aabfb2441916f3285ca64c8f709d667da
ctwillson <anti.c@foxmail.com>
2021-03-13 23:34:50 +0800

    update add stock

--- addstock.py
+++ addstock.py
@@ -1,10 +1,24 @@
 import pysnowball as ball
 import time
-with open('./testdata/stock/test.txt', 'r') as file_object:
+import os
+import datetime
+import sys
+import pandas as pd
+dir_path = os.path.dirname(os.path.abspath(__file__))
+cal = str(datetime.date.today().strftime('%m%d'))
+
+print(cal)
+with open('./testdata/stock/attention.txt', 'r') as file_object:
     stock_list = file_object.readlines()
-for name in stock_list:
-    symbol = name[-3:-1]+name[11:17]
+for symbol in stock_list:
+    symbol = symbol[11:19]
     print(symbol)
     ball.private_pick(symbol)
-    ball.private_packet(symbols=symbol,packet_name='test')
+    ball.private_packet(symbols=symbol,packet_name='zg'+cal)
     time.sleep(0.1)
+# df = pd.read_csv(dir_path+'/testdata/attention/test.csv')
+# stock_list = df.loc[:,'ts_code']
+# price_zg = df.loc[:,'last_zg']
+# for index,symbol in enumerate(stock_list):
+#     down = price_zg[index] + 0.05
+#     ball.price_alert(symbol=symbol,price_desc = down)
