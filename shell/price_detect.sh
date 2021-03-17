4e6833189c17f8fda131eea62742cc2badafdf81
ctwillson <anti.c@foxmail.com>
2021-03-18 00:17:52 +0800

    for tenxun cloud enconding error

--- shell/price_detect.sh
+++ shell/price_detect.sh
@@ -6,7 +6,8 @@ export TS_TOKEN=''
 export XUEQIULOGINCOOKIE=''
 #put your pushplus
 export PUSHPLUSTOKEN=''
-
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 CURDIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd )
 PYTHONDIR=$(which python)
 $PYTHONDIR $CURDIR/../price_dect.py
