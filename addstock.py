b644ecee64114eb391b9121ae3127672390cb209
ctwillson <anti.c@foxmail.com>
2021-03-18 23:49:09 +0800

    update addstock

--- addstock.py
+++ addstock.py
@@ -16,6 +16,15 @@ for symbol in stock_list:
     ball.private_pick(symbol)
     ball.private_packet(symbols=symbol,packet_name='zg'+cal)
     time.sleep(0.1)
+
+with open('./testdata/stock/attention_add.txt', 'r') as file_object:
+    stock_list = file_object.readlines()
+for symbol in stock_list:
+    symbol = symbol[11:19]
+    print(symbol)
+    ball.private_pick(symbol)
+    ball.private_packet(symbols=symbol,packet_name='zgadd'+cal)
+    time.sleep(0.1)
 # df = pd.read_csv(dir_path+'/testdata/attention/test.csv')
 # stock_list = df.loc[:,'ts_code']
 # price_zg = df.loc[:,'last_zg']
