5413cbc8403be4c1374ae4924ccc47abb92da980
ctwillson <anti.c@foxmail.com>
2021-04-19 00:03:56 +0800

    bug fix for pd_kill.sh

--- shell/pd_kill.sh
+++ shell/pd_kill.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-prossname=price_detect.sh
-
+prossname=price_dect
 ps -aux | grep ${prossname} | grep -v grep |awk '{print $2}' | while read pid
-
 do
-    echo "front is running, to kill front pid=$pid"
-    kill -9 $pid
-    echo "kill result: $?"
+	echo "front is running, to kill front pid=$pid"
+	kill -9 $pid
+	echo "kill result: $?"
 done
